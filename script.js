0e3ae382ec315a1bb82afc15472beb04e6188dea
Utopia <utopiaedgeit@gmail.com>
2022-10-11 13:29:36 -0400

    Created more elements

---
+++ script.js
@@ -0,0 +1,6 @@
+/*const APIURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+function getWeatherByLocation() {
+const resp = await fetch()
+}
+getWeatherByLocation('Ethiopia');
+*/
