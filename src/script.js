10b34eaf0e8530bbd499120de93d78386a583c65
Utopia <utopiaedgeit@gmail.com>
2022-10-14 01:00:32 -0400

    Created unit conversion

--- src/script.js
+++ src/script.js
@@ -20,12 +20,14 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    cesiusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt*1000);
+    dateElement.innerHTML = formateDate(response.data.dt*1000);
     iconElement.innerHTML = `http://openweathermap.org/img/wn/0${response.data.weather[0].icon}@2x.png`;
 }
 function search(city)
@@ -41,10 +43,35 @@ function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-    console.log(cityInputElement);
+    //console.log(cityInputElement);
 }   
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    //remove the active class the celsius link
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (cesiusTemperature * 9) /5 * 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
-search("Ethiopia");
+}
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(cesiusTemperature);
+}
+
+let cesiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("Ethiopia");
