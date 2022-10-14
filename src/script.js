a3be26e16aa2e30730c0c406133dd7a053e7e3f3
Utopia <utopiaedgeit@gmail.com>
2022-10-14 01:49:30 -0400

    Edit some typo errors

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
 //const { default: axios } = require("axios");
-function formateDate(timestamp) {
+function formatDate(timestamp) 
+{
     //calculate the date
     let date = new Date(timestamp);
-    let hours = temestamp.getHours();
+    let hours = date.getHours();
+    if (hours < 10) { hours = `0${hours}`;}
     let minutes = date.getMinutes();
+    if (minutes < 10) { minutes = `0${minutes}`;}
     let days = ["Sunday", "Monday", "Tuesday", "Wedensday", "Thursday", "Friday", "Saturday"];
     let day = date.getDay();
    
-        return `"{day} ${hours}: ${minutes}`;
+        return `${day} ${hours}: ${minutes}`;
 }
 
-function displayTemperature(response) {
-    console.log(response.data.main.temp);
+function displayTemperature(response) 
+{
+   // console.log(response.data.main.temp);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
@@ -20,38 +24,39 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
-    cesiusTemperature = response.data.main.temp;
+    celsiusTemperature = response.data.main.temp;
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.humidity;
+    humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formateDate(response.data.dt*1000);
-    iconElement.innerHTML = `http://openweathermap.org/img/wn/0${response.data.weather[0].icon}@2x.png`;
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function search(city)
 {
 let apikey = "3ce33118eff34374ba718f22cafc2913";
 let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-let city = "Ethiopia";
-//console.log(apiurl);
 axios.get(apiurl).then(displayTemperature);
 }
 
-function handleSubmit(event) {
+function handleSubmit(event) 
+{
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-    //console.log(cityInputElement);
 }   
-function displayFahrenheitTemperature(event) {
+function displayFahrenheitTemperature(event) 
+{
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
     //remove the active class the celsius link
     celsiusLink.classList.remove("active");
     fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (cesiusTemperature * 9) /5 * 32;
+    let fahrenheitTemperature = (celsiusTemperature * 9) /5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
 }
@@ -60,10 +65,10 @@ function displayCelsiusTemperature(event) {
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(cesiusTemperature);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let cesiusTemperature = null;
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
@@ -72,6 +77,6 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+celsiusLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("Ethiopia");
